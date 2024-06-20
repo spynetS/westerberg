92532a40acbdb9cf54ac7d9d926ffe93c2235c07
Alfred Roos <alfred@stensatter.se>
2024-06-20 07:05:35 +0200

    added more failsafe and fixed rememeber input on servicereport

--- servicereport/views.py
+++ servicereport/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+import re
 from django.shortcuts import render
 
 from .models import ServiceReport
@@ -6,21 +7,32 @@ from .models import ServiceReport
 # Create your views here.
 
 def create_report(request):
+   reqs = {}
+   empty = False
    for item in request.POST:
+      reqs[item] = request.POST[item]
       if request.POST[item] == "":
-         return render(request, "servicereport/form.html",{"error":f"{item.title()} Inte i fyllt. Fyll i alla!"})
+         empty = True
+         print(reqs)
+   if empty:
+      return render(request, "servicereport/form.html",dict({"error":f"{item.title()} Inte i fyllt. Fyll i alla!"}.items()|reqs.items()))
 
+   reg = re.compile("\d{3}[ ]?\d{2}")
+   if not reg.match(request.POST["Postnummer"]):
+      return render(request, "servicereport/form.html",dict({"error":f"Postnummer format stämmer inte. 123 45 eller 12345"}.items()|reqs.items()))
+
+   
    try:
       report = ServiceReport(
          name=request.POST['Namn'],
-         email=request.POST['E-post'],
+         email=request.POST['Epost'],
          phone=request.POST['Telefonnummer'],
          adress=request.POST['Adress'],
          postnumber=request.POST['Postnummer'],
          area=request.POST['Ort'],
          homedate=request.POST['Hemmadatum'],
-         homefrom=request.POST['Hemma fr.o.m. kl'],
-         hometo=request.POST['Hemma t.o.m. kl'],
+         homefrom=request.POST['Hemmafrom'],
+         hometo=request.POST['Hemmato'],
          description =request.POST['Felbeskrivning'],
       )
       report.save()
